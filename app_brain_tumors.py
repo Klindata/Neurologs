519d88ef25ceda50f11c85626bc94fc9bcb58d0c
Klindata <75170291+Klindata@users.noreply.github.com>
2021-05-22 21:07:59 +0100

    Update app_brain_tumors.py

--- app_brain_tumors.py
+++ app_brain_tumors.py
@@ -36,11 +36,13 @@ title = components.html("""<div><h1 style="color:#9CFF8B; font-size:230%; text-a
 image_app1 = Image.open("photo/image.jpg")                        
 st.image(image_app1, use_column_width=True)
 
+st.write(" ")
+
 st.write("We have designed a deep learning model (convolutional neural network) which can detect and classify the most common primary brain tumors : glioma, meningioma and pituitary tumors.")
 
 st.write("The model has been trained with more than 3200 MRI scans images and predictions are correct 95% of the time.")
          
-st.write("Please test our medical diagnosis tool : upload a brain scan and obtain the result in only a minute !")
+st.write("Please test our medical diagnosis tool. Upload a brain scan and obtain the result in only a minute !")
 
 #presentation = components.html("""<div style="color:white; font-size:108%"><br><p>We have designed a deep learning model (convolutional neural network) which can detect and classify 
                                #the most common primary brain tumors: glioma, meningioma and pituitary tumors.</p>
